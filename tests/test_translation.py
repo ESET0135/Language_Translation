ea20cc9066fcb7296a6a136f603732f459bfee74
ESET0135 <Shubham.joshi@esyasoft.com>
2025-10-23 10:39:30 +0530

    Tests test

--- tests/test_translation.py
+++ tests/test_translation.py
@@ -1,3 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from language_mapping import language_codes
+
 # test_translation.py
 from language_mapping import language_codes
 
